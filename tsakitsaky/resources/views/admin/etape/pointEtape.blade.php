1a76ea59a2e5f2b49c2575f78efcef1d514b154f
fanyahMi <119622786+fanyahMi@users.noreply.github.com>
2024-07-04 02:26:17 +0200

    denier
    zay

--- tsakitsaky/resources/views/admin/etape/pointEtape.blade.php
+++ tsakitsaky/resources/views/admin/etape/pointEtape.blade.php
@@ -5,7 +5,7 @@
           <div class="bgc-white bd bdrs-3 p-20 mB-20">
               <div class="row">
                   <div class="col-md-6">
-                      <h1 class="c-grey-900 mB-20">Point obtenue par etape</h1>
+                      <h1 style="font-size: 20px" class="c-grey-900 mB-20">Point obtenue par etape</h1>
                   </div>
                   <div class="col-md-2"></div>
               </div>
